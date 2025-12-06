ab0e27ca7076122622d07caa23625b4d664552b1
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-12-05 21:19:03 -0300

    Update settings.py

--- catequese26/catequese26/settings.py
+++ catequese26/catequese26/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'catequese26.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'catequese.sqlite3',
     }
 }
 
