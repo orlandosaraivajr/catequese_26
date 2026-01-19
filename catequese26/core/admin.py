d15d0e6e2b09343a0ab68e623ecde464cc46c862
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2026-01-19 14:31:56 -0300

    Update admin.py

--- catequese26/core/admin.py
+++ catequese26/core/admin.py
@@ -3,6 +3,8 @@ from core.models import CatequeseAdultoModel
 from core.models import CatequeseInfantilModel
 from core.models import CrismaModel
 from core.models import Perseveranca_MEJ_Model
+from core.models import NoivoModel
+from core.models import CoroinhaModel
 
 
 class CatequeseInfantilAdmin(admin.ModelAdmin):
@@ -25,7 +27,18 @@ class Perseveranca_MEJ_Admin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
     search_fields = ('nome','data_nascimento','criado_em')
 
+class NoivoAdmin(admin.ModelAdmin):
+    list_display = ('nome_noivo','nome_noiva','data_provavel_casamento','criado_em')
+    date_hierarchy = 'criado_em'
+    search_fields = ('nome_noivo','nome_noiva','data_provavel_casamento','criado_em')
+class CoroinhaAdmin(admin.ModelAdmin):
+    list_display = ('nome','data_nascimento','criado_em')
+    date_hierarchy = 'criado_em'
+    search_fields = ('nome','data_nascimento','criado_em')
+        
 admin.site.register(CatequeseInfantilModel, CatequeseInfantilAdmin)
 admin.site.register(CatequeseAdultoModel, CatequeseAdultoAdmin)
 admin.site.register(CrismaModel, CrismaAdmin)
 admin.site.register(Perseveranca_MEJ_Model, Perseveranca_MEJ_Admin)
+admin.site.register(NoivoModel, NoivoAdmin)
+admin.site.register(CoroinhaModel, CoroinhaAdmin)
