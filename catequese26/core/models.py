30ade06aa8757dc488e8c3cd96597f4aacbb6480
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2026-02-20 19:48:39 -0300

    Update models.py

--- catequese26/core/models.py
+++ catequese26/core/models.py
@@ -9,11 +9,11 @@ class CatequeseInfantilModel(models.Model):
     
     HORARIO_CATEQUESE = (
 #        ("1", "Pré-Catequese - Segunda às 19:30h"),
-#        ("2", "Pré-Catequese - Terça às 19:30h"),
+        ("2", "Pré-Catequese - Terça às 19:30h"),
 #        ("3", "Pré-Catequese - Sábado às 09h"),
 #        ("4", "Pré-Catequese - Sábado às 10:30h"),
 #        ("5", "1a Etapa - Segunda às 19:30h"),
-        ("6", "1a Etapa - Terça às 19:30h"),
+#        ("6", "1a Etapa - Terça às 19:30h"),
         ("7", "1a Etapa - Quarta às 19:30h"),
 #        ("8", "1a Etapa - Sábado às 09h"),
         ("9", "1a Etapa - Sábado às 10:30h"),
